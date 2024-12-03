efdea815c90659d1c9c5429246e3f7fd63476ca9
Stefano Righini <stefano.righini@quinck.io>
2024-12-03 22:39:02 +0100

    feat: ✨ complete third day second star

--- rust-3/src/main.rs
+++ rust-3/src/main.rs
@@ -3,14 +3,43 @@ use std::fs::File;
 use std::io::{self, Read};
 
 const MUL_REGEX: &str = r"mul\((\d{1,3}),(\d{1,3})\)";
+const DO: &str = "do()";
+const DONT: &str = "don't()";
 
 fn main() {
-    let input = read_input("res/input.txt").expect("String inputfile was expected");
+    let input = read_input("res/input.txt").expect("String input file was expected");
 
-    let re: Regex = Regex::new(MUL_REGEX).unwrap();
+    let muls_sum = sum_mul(&input);
 
-    let muls_sum: usize = re
-        .captures_iter(&input)
+    match muls_sum {
+        Ok(muls_sum) => println!("The results of the multiplications: {}", muls_sum),
+        Err(error) => println!("{}", error.to_string()),
+    }
+
+    let do_muls_sum: usize = input
+        .split(DO)
+        .flat_map(|v| v.split(DONT).next())
+        .flat_map(|s| sum_mul(s).ok())
+        .sum();
+
+    println!("The result obtained adding up all of the results of just the enabled multiplications is: {}", do_muls_sum);
+}
+
+// 184511516
+//  90044227
+
+fn read_input(path: &str) -> Result<String, io::Error> {
+    let mut file = File::open(path)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn sum_mul(input: &str) -> Result<usize, regex::Error> {
+    let mul_regex = Regex::new(MUL_REGEX)?;
+
+    let result: usize = mul_regex
+        .captures_iter(input)
         .flat_map(|c| {
             let (_, [f1, f2]) = c.extract();
             let f1 = f1.parse::<usize>().ok();
@@ -22,12 +51,5 @@ fn main() {
         })
         .sum();
 
-    println!("The results of the multiplications: {}", muls_sum);
-}
-
-fn read_input(path: &str) -> Result<String, io::Error> {
-    let mut file = File::open(path)?;
-    let mut content = String::new();
-    file.read_to_string(&mut content)?;
-    Ok(content)
+    Ok(result)
 }
